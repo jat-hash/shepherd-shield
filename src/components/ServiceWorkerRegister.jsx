93e0a0aa2dfee2a45093986bd82dfc675a62c705
James Lim <limstarservices@gmail.com>
2026-02-22 23:47:32 +0000

    File changes

--- src/components/ServiceWorkerRegister.jsx
+++ src/components/ServiceWorkerRegister.jsx
@@ -5,9 +5,9 @@ import { getFCMToken } from "./firebase";
 
 export default function ServiceWorkerRegister() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
+    if ('serviceWorker' in navigator && 'Notification' in window) {
+          navigator.serviceWorker
+            .register('/service-worker.js', { scope: '/' })
         .then(async (registration) => {
           console.log('Service Worker registered - app will run in background');
 
